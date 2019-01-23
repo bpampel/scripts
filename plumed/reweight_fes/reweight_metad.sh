95ad57b3c6c0eb462adf0d9edea1a652ecda1403
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2019-01-23 17:23:53 +0100

    Some script for mass reweighting of CVs

--- plumed/reweight_fes/reweight_metad.sh
+++ plumed/reweight_fes/reweight_metad.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-# has to be set correctly to match the FES output stride
-timestep=5
+plumedfile='/home/theorie/pampel/scripts/plumed/reweight_fes/plumed.dat'
 
-
-if [ "$#" -ne 2 ]; then
-  echo "Wrong number of arguments.
-        Usage:
-        reweight_fes.sh $scriptfile $directory"
-fi
-
-
-plumedfile=$(realpath $1)
+refdir="/usr/data/pampel/averaging/chebyshev_ord20/1"
+cd $refdir
+feslist=`ls fes*`
 
 # first create the basic structure
-rootdir=$2
+rootdir=$1
 cd $rootdir
 echo 'Currently working in '`pwd`
-filenum=$(ls fes* | wc -l) # just to monitor progress
-counter=0
 mkdir 'reweighting'
 cd 'reweighting'
 for fes in $( ls ../fes* )
@@ -27,9 +18,9 @@ do
   # create dir and move files
   mkdir $time
   cd $time
-  last_line=`expr $time \* $timestep + 3`
-  sed "$last_line"',$d' ../../colvar.data > colvar.data
-  cp $plumedfile plumed.dat
+  last_line=`expr $time \* 5 + 3`
+  sed "$last_line"',$d' ../../COLVAR > COLVAR
+  cp $plumedfile .
   # run plumed and move output to parent directory
   plumed --no-mpi driver --noatoms > /dev/null
   cp 'fes' $fes # variable already contains ../
