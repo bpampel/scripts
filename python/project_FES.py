0221b70d2f5f7c1147d2c2473bed513b77ffcbb3
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2019-08-26 16:36:12 +0200

    Default outfile name should work now
    started adding the number parser

--- python/project_FES.py
+++ python/project_FES.py
@@ -5,6 +5,7 @@ Currently only 2d -> 1d
 """
 
 import argparse
+from os import path
 import numpy as np
 
 
@@ -21,16 +22,58 @@ def parse_args():
                         help="Energy (in units of kT) of the FES file",
                         required=True)
     parser.add_argument("-o", "--outfile",
-                        help="Name of the output file",
-                        default="proj_fes")
+                        help="Name of the output file")
 
     args = parser.parse_args()
 
-    # works not with absolute paths
-    # if args.outfile is None:
-        # args.outfile = "proj_" + str(args.dim) + "_" +  args.filename
+    if args.outfile is None:
+        directory = ''
+        splitted_filename = args.filename.split('/')
+        if len(splitted_filename) > 1:
+            directory = '/'.join(splitted_filename[:-1]) + "/"
+        args.outfile = directory + "proj_" + str(args.dim) + "_" +  splitted_filename[-1]
 
     return args
+
+
+def number_format_parser(number):
+    """Get the format of a number string"""
+    # all variables here, could be turned into module
+    fmt_char = 'f'
+    is_negative = False
+    int_length = 0
+    dec_length = 0
+
+    try:
+        float(number)
+    except ValueError:
+        print('String is not a number')
+
+    if number[0] == '-':
+        is_negative = True
+
+    dot_position = number.find('.')
+    if dot_position == -1:
+        fmt_str = "%{}.0f".format(len(number) - is_negative)
+        return fmt_str
+
+    int_length = dot_position - is_negative
+
+    # check if exponential if dot was in 2nd (with minus 3rd) character
+    if dot_position - is_negative == 1:
+        for e in ['e', 'E']:
+            e_position = number.find(e)
+            if e_position != -1:
+                fmt_char = e
+                dec_length = e_position - dot_position - 1
+                break
+
+    if fmt_char == 'f':
+        dec_length = len(number) - dot_position - 1
+
+    fmt_str = "%{}.{}{}".format(int_length, dec_length, fmt_char)
+    return fmt_str
+
 
 
 
@@ -43,6 +86,13 @@ def parse_args():
         # else:
             # return header
 
+def get_number_format(filename):
+    """Get number format of file from the first number"""
+    for line in open(filename):
+        if line.startswith('#'):
+            continue
+        else:
+            return number_format_parser(line.split()[2])
 
 
 if __name__ == '__main__':
@@ -50,6 +100,7 @@ if __name__ == '__main__':
     args = parse_args()
 
     fes = np.genfromtxt(args.filename)
+    number_format = get_number_format(args.filename)
 
     num_dim1 = np.where(fes[:, 0] == fes[0, 0])[0][1] # via periodicity
     num_dim2 = int(fes.shape[0] / num_dim1)
@@ -72,7 +123,7 @@ if __name__ == '__main__':
     projected_fes -= np.min(projected_fes)
 
     np.savetxt(args.outfile, np.asmatrix(np.vstack((cv_values, projected_fes)).T),
-               fmt='%1.16f', delimiter=' ', newline='\n')
+               fmt=number_format, delimiter=' ', newline='\n')
 
 # else:
     # exit(-1)
