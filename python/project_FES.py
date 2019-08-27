b066c95553635543124f51561544b0c551093069
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2019-08-27 11:04:11 +0200

    Added submodule, fixed project_FES to work with number parser

--- python/project_FES.py
+++ python/project_FES.py
@@ -7,6 +7,7 @@ Currently only 2d -> 1d
 import argparse
 from os import path
 import numpy as np
+from helpers import number_format as nfmt
 
 
 def parse_args():
@@ -36,43 +37,6 @@ def parse_args():
     return args
 
 
-def number_format_parser(number):
-    """Get the format of a number string"""
-    # all variables here, could be turned into module
-    fmt_char = 'f'
-    is_negative = False
-    int_length = 0
-    dec_length = 0
-
-    try:
-        float(number)
-    except ValueError:
-        print('String is not a number')
-
-    if number[0] == '-':
-        is_negative = True
-
-    dot_position = number.find('.')
-    if dot_position == -1:
-        fmt_str = "%{}.0f".format(len(number) - is_negative)
-        return fmt_str
-
-    int_length = dot_position - is_negative
-
-    # check if exponential if dot was in 2nd (with minus 3rd) character
-    if dot_position - is_negative == 1:
-        for e in ['e', 'E']:
-            e_position = number.find(e)
-            if e_position != -1:
-                fmt_char = e
-                dec_length = e_position - dot_position - 1
-                break
-
-    if fmt_char == 'f':
-        dec_length = len(number) - dot_position - 1
-
-    fmt_str = "%{}.{}{}".format(int_length, dec_length, fmt_char)
-    return fmt_str
 
 
 
@@ -86,13 +50,13 @@ def number_format_parser(number):
         # else:
             # return header
 
-def get_number_format(filename):
-    """Get number format of file from the first number"""
+def get_number_string(filename):
+    """Get number string of file from the first number line"""
     for line in open(filename):
         if line.startswith('#'):
             continue
         else:
-            return number_format_parser(line.split()[2])
+            return line.split()[2]
 
 
 if __name__ == '__main__':
@@ -100,7 +64,8 @@ if __name__ == '__main__':
     args = parse_args()
 
     fes = np.genfromtxt(args.filename)
-    number_format = get_number_format(args.filename)
+    fmt = nfmt.NumberFmt()
+    fmt.parse(get_number_string(args.filename))
 
     num_dim1 = np.where(fes[:, 0] == fes[0, 0])[0][1] # via periodicity
     num_dim2 = int(fes.shape[0] / num_dim1)
@@ -123,7 +88,7 @@ if __name__ == '__main__':
     projected_fes -= np.min(projected_fes)
 
     np.savetxt(args.outfile, np.asmatrix(np.vstack((cv_values, projected_fes)).T),
-               fmt=number_format, delimiter=' ', newline='\n')
+               fmt=fmt.get(), delimiter=' ', newline='\n')
 
 # else:
     # exit(-1)
