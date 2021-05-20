4dea4a0d6edc9d00636c9efaa6a5d4711846ed35
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2021-05-20 12:14:49 +0200

    Modify project_fes to allow row-major column sorting in 2d

--- python/project_fes.py
+++ python/project_fes.py
@@ -25,6 +25,8 @@ def parse_args():
                         required=True)
     parser.add_argument("-o", "--outfile",
                         help="Name of the output file")
+    parser.add_argument("--row-major", action="store_true", dest="row_major",
+                        help="Array is row-major instead of column major (Last index changes fastest)")
 
     args = parser.parse_args()
 
@@ -40,12 +42,12 @@ def manipulate_header(header, dim):
     Remove everything from the projected out dimension
     """
     proj_variable = header.fields[dim]
-    removed_variable = header.fields[3-dim]  # assuming 2d FES
-    value_field = header.fields[3]
+    removed_variable = header.fields[2-dim]  # assuming 2d FES
+    value_field = header.fields[2]
     header.fields = ["projection." + proj_variable, value_field]
-    for const in header.const:  # remove constants related to projected out variable
-        if removed_variable in const:
-            del header.const[const]
+    remove_const = [const for const in header.constants if removed_variable in const]
+    for const in remove_const: # remove constants related to projected out variable
+        del header.constants[const]
 
 
 if __name__ == '__main__':
@@ -59,16 +61,24 @@ if __name__ == '__main__':
     manipulate_header(header, args.dim)
 
 
-    num_dim1 = np.where(fes[:, 0] == fes[0, 0])[0][1] # via periodicity
-    num_dim2 = int(fes.shape[0] / num_dim1)
-
-    # get CV values of correct dimension
-    if args.dim == 1:
-        cv_values = fes[:num_dim1, 0]
-    elif args.dim == 2:
-        cv_values = fes[::num_dim1, 1]
+    if not args.row_major:  # column-major as in plumed
+        num_dim1 = np.where(fes[:, 0] == fes[0, 0])[0][1] # via periodicity
+        num_dim2 = int(fes.shape[0] / num_dim1)
+        if args.dim == 1:
+            cv_values = fes[:num_dim1, 0]
+        elif args.dim == 2:
+            cv_values = fes[::num_dim1, 1]
+        else:
+            raise ValueError("Dimension must be either 1 or 2")
     else:
-        raise ValueError("Dimension must be either 1 or 2")
+        num_dim2 = np.where(fes[:, 1] == fes[0, 1])[0][1] # via periodicity
+        num_dim1 = int(fes.shape[0] / num_dim2)
+        if args.dim == 1:
+            cv_values = fes[::num_dim1, 0]
+        elif args.dim == 2:
+            cv_values = fes[:num_dim1, 1]
+        else:
+            raise ValueError("Dimension must be either 1 or 2")
 
     # put values in matrix, dim2 is in the rows (because fes from plumed is column major)
     fesmatrix = fes[:, 2].reshape(num_dim2, num_dim1)
