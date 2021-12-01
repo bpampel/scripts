caadc6b8c5a35f51148a5c2db4c8c10697c26e64
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2021-12-01 17:05:56 +0100

    Allow multiple walker simulation

--- python/effective_sample_size.py
+++ python/effective_sample_size.py
@@ -8,8 +8,8 @@ import numpy as np
 def parse_args():
     """Get cli args"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("path",
-                        help="Path to the colvar file")
+    parser.add_argument("path", nargs='+',
+                        help="Path to the colvar file(s)")
     parser.add_argument("-c", "--biascol", type=int,
                         help="Column of the file containing the bias values")
     parser.add_argument("-t", "--thermalenergy", type=float,
@@ -31,27 +31,36 @@ def calc_normalized_weights(biasvals, kt):
     return np.exp((biasvals - biasmax) / kt)
 
 
-def calc_eff_sample_size(filepath, biascol, kt):
-    """Calculate the effective sample size of a colvar file
+def analyze_simulation(filepath, biascol, kt):
+    """Calculate the effective sample size and statistics of the weights
 
-    param filepath: path to colvar file
+    param filepath: path to colvar file, or list of filepaths for multiple walkers
     param biascol: column of file containing the bias values
     param kt: thermal energy of simulation
-    return: effective sample size
+    returns: (effective_sample_size, weights_mean, weights_stddev)
     """
-    biasvals = np.genfromtxt(filepath)[:, biascol]
+    if isinstance(filepath, list):
+        biasvals = []
+        for f in filepath:
+            biasvals.append(np.genfromtxt(f)[:, biascol])
+        biasvals = np.concatenate(biasvals)
+    else:
+        biasvals = np.genfromtxt(filepath)[:, biascol]
     weights = calc_normalized_weights(biasvals, kt)
-    return eff_sample_size_from_weights(weights)
+    weights_mean = np.mean(weights)
+    weights_stddev = np.std(weights, ddof=1)
+    eff_sample_size = eff_sample_size_from_weights(weights)
+    return (eff_sample_size, weights_mean, weights_stddev)
 
 
 def main():
     args = parse_args()
-    sample_size = calc_eff_sample_size(
+    sample_size, w_mean, w_stddev = analyze_simulation(
         args.path,
         args.biascol - 1,  # python cols start from 0
         args.thermalenergy,
     )
-    print(sample_size)
+    print(f"{sample_size}    {w_mean}    {w_stddev}")
 
 
 if __name__ == "__main__":
